94b563b8ee4d16cc8b9b143f6f2333732b899d6d
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-29 20:09:33 -0300

    Finish the FaceId and the TouchID verification in the app

--- BucketList/View/ContentView.swift
+++ BucketList/View/ContentView.swift
@@ -5,43 +5,41 @@
 //  Created by Orlando Moraes Martins on 29/12/22.
 //
 
+import LocalAuthentication
 import SwiftUI
-import MapKit
-
-struct Location: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
-}
 
 struct ContentView: View {
-    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: -15.893, longitude: -52.2599), span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
-    
-    let locations = [
-        Location (name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude:
-        51.501, longitude: -0.141)),
-        Location (name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude:
-        51.508, longitude: -0.076))
-    ]
+    @State private var isUnlocked = false
     
     var body: some View {
-        NavigationView{
-            Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
-                MapAnnotation(coordinate: location.coordinate) {
-                    NavigationLink {
-                        Text(location.name)
-                    } label: {
-                        VStack{
-                            Circle()
-                                .stroke(.red, lineWidth: 3)
-                                .frame(width: 44, height: 44)
-                            
-                            Text(location.name)
-                        }
-                    }
+        VStack {
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
+            }
+        }
+        .onAppear(perform: authenticate)
+        .padding()
+    }
+    
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+            let reason = "We need to unlock your data"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason ){ success, authenticationError in
+        
+                if success {
+                    isUnlocked = true
+                } else {
+                    //there was a problem
                 }
             }
-            .navigationTitle("Map Explorer")
+        } else {
+            // no biometrics
         }
     }
 }
