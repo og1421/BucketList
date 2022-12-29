411435845dda7cb4fda30f5beae461506820220a
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2022-12-29 17:22:29 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Orlando Moraes Martins on 29/12/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
