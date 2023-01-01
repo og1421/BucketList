9c2c66a7aa4257ae77dfb69c63658d95f8d3aa57
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-01 16:14:46 -0300

    Using Biometry to unlock our app

--- BucketList/Model/ContentView-ViewModel.swift
+++ BucketList/Model/ContentView-ViewModel.swift
@@ -6,15 +6,15 @@
 //
 
 import Foundation
+import LocalAuthentication
 import MapKit
 
 extension ContentView {
     @MainActor class ViewModel: ObservableObject {
         @Published var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 50, longitude: 0), span: MKCoordinateSpan(latitudeDelta: 25, longitudeDelta: 25))
-        
         @Published private (set) var locations: [Location]
-        
         @Published var selectedPlace: Location?
+        @Published var isUnlocked = false
         
         let savePath = FileManager.documentsDirectory.appendingPathComponent("SavedPlaces")
         
@@ -48,6 +48,29 @@ extension ContentView {
             if let index = locations.firstIndex(of: selectedPlace) {
                 locations[index] = location
                 save()
+            }
+        }
+        
+        func authenticate() {
+            let context = LAContext()
+            var error: NSError?
+            
+            if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+                let reason = "Please authenticate yourself to unlock your places"
+                
+                context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                    success, authenticationError in
+                    
+                    if success {
+                        Task { @MainActor in
+                            self.isUnlocked = true
+                        }
+                    } else {
+                        //error 
+                    }
+                }
+            } else {
+                //no biometrics
             }
         }
     }
